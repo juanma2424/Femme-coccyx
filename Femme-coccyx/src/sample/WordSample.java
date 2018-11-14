1d9009f5d318b72fbf9eb4929af75b460a096352
SexuallyActiveToyotaPrius <42818913+SexuallyActiveToyotaPrius@users.noreply.github.com>
2018-11-14 17:05:05 -0600

    TXT Parsing ready

---
+++ Femme-coccyx/src/sample/WordSample.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class WordSample {
+    
+    public int region;
+    private String word;
+    public int counter;
+    
+    public WordSample(int region, String word){
+        this.region = region;
+        this.word = word;
+        this.counter = 1;
+    }
+    
+    public void increaseCounter(){
+        this.counter++;
+    }
+    
+}
