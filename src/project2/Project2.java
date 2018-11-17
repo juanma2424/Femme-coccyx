cb47bf1e1e60f3949ae489f76030a5072f548543
juanma2424 <42756436+juanma2424@users.noreply.github.com>
2018-11-17 11:40:36 -0600

    merge

---
+++ src/project2/Project2.java
@@ -0,0 +1,37 @@
+package project2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import parsing.TXTParser;
+import sample.WordSample;
+import structures.BPTree;
+
+public class Project2 {
+
+    public static void main(String[] args) {
+        TXTParser parser = new TXTParser();
+        parser.setDirec("src\\books\\ThePrince.txt");
+        //System.out.println(parser.getTXTsize());
+        BPTree<String, BPTree<String, WordSample>> tree = parser.getBTree(6, 6, 1, 9);
+        System.out.println(tree.search("p").search("prince").counter);
+        System.out.println(tree.search("a").search("asombroso").region);
+        
+        try {
+            Scanner scan = new Scanner(new File("src\\books\\ThePrince.txt"));
+            int n = 0;
+            while (scan.hasNext()){
+                String a = scan.next().toLowerCase();
+                if ("asombroso".equals(a)){
+                    n++;
+                }
+            }
+            System.out.println("Las veces que aparece asombroso es: " + n);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Project2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
