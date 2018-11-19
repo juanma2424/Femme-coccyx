61175f013be13b8b67cab0782b40dc623267d1ab
juanma2424 <42756436+juanma2424@users.noreply.github.com>
2018-11-19 05:42:49 -0600

    carga todas las img

--- src/RGBSample/oneID.java
+++ src/RGBSample/oneID.java
@@ -1,4 +1,3 @@
-
 package RGBSample;
 
 import java.util.ArrayList;
@@ -6,45 +5,44 @@ import java.util.Objects;
 import sample.pixelSample;
 
 public class oneID {
-    
+
     public ArrayList<pixelSample> ONEIDS = new ArrayList<>();
     ArrayList<Integer> IDSFINAL = new ArrayList<>();
-   
-    
-    public void imp(){
+
+    public void imp() {
         for (int i = 0; i < ONEIDS.size(); i++) {
             System.out.println(ONEIDS.get(i).getId());
         }
     }
-    
-   
-    public void ID_One(){
-        for (int i = 0; i <  ONEIDS.size(); i++){
-            for (int j =0; j < ONEIDS.size(); j++){
-                if (i != j && ( Objects.equals( ONEIDS.get(i),ONEIDS.get(j)))){
-                    ONEIDS.remove(j);
-                    ONEIDS.get(i).setCantidad((ONEIDS.get(i).getCantidad()+1));
+
+    public void ID_One() {
+        for (int i = 0; i < ONEIDS.size(); i++) {
+            for (int j = 0; j < ONEIDS.size(); j++) {
+                if (i != j && (Objects.equals(ONEIDS.get(i), ONEIDS.get(j)))) {
+                    try {
+                        ONEIDS.remove(j);
+                        ONEIDS.get(i).setCantidad(((ONEIDS.get(i).getCantidad()) + (1)));
+                    } catch (Exception e) {
+
+                    }
+
                 }
             }
         }
     }
-    
-    
-    public void checkRe(){
-        for (int i = 0; i <  ONEIDS.size(); i++){
-            for (int j =0; j < ONEIDS.size(); j++){
-                if (i != j && ( Objects.equals( ONEIDS.get(i),ONEIDS.get(j)))){
+
+    public void checkRe() {
+        for (int i = 0; i < ONEIDS.size(); i++) {
+            for (int j = 0; j < ONEIDS.size(); j++) {
+                if (i != j && (Objects.equals(ONEIDS.get(i), ONEIDS.get(j)))) {
                     System.out.println("hay repetidos");
                 }
             }
         }
     }
-  
-    
-    
-    
-    public void one(ArrayList<pixelSample> pID){
+
+    public void one(ArrayList<pixelSample> pID) {
         this.ONEIDS = pID;
         ID_One();
-    }    
+    }
 }
