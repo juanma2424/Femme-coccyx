a41bc6c134ff3e018dbe9df7c065d72e70f633fe
juanma2424 <42756436+juanma2424@users.noreply.github.com>
2018-11-20 21:53:32 -0600

    tags

--- src/Thread/mainT.java
+++ src/Thread/mainT.java
@@ -6,28 +6,21 @@ package Thread;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import sample.pixelSample;
 
 public class mainT {
     
     
 
-//    public static void main(String[] args) {
-//        
-//        ArrayList<ImagePixel> Pixels = new ArrayList<ImagePixel>();
-//        Pixels.add(new ImagePixel());
-//       
-//        //<>,name,nomlist
-//        ExecutorService executor = Executors.newFixedThreadPool(3);
-//        for (ImagePixel sample: Pixels) {
-//            Runnable sector = new sacanRegion(sample);
-//            executor.execute(sector);
-//        }
-//        executor.shutdown();	// Cierro el Executor
-//        while (!executor.isTerminated()) {
-//        	// Espero a que terminen de ejecutarse todos los procesos 
-//        	// para pasar a las siguientes instrucciones 
-//        }
-//    }
-       
+    public static void main(String[] args) {
+    int M = 1;
+    int N =  9;
+    for (int i = 0; i < (1048576) ; i++) {
+            
+            int num = (int) Math.floor(Math.random() * (N - M +1) + M);
+            System.out.println(num);
+             
+        }      
 }
 
+}
--- src/percentage/extractPixel.java
+++ src/percentage/extractPixel.java
@@ -7,29 +7,37 @@ public class extractPixel {
 
     public ArrayList<pixelSample> datos(Hashtable table, int limt, int lims) {
         ArrayList<pixelSample> Extrac = new ArrayList<>();
-       
-        int M = lims - 3;
-        int N = limt  - 3;
+       int N ;
+       int M;
+        if(limt>=lims){
+         N = limt  ;
+         M = lims ;
+        }else{
+             M = limt  ;
+             N = lims ;
+        }
+        
+        
         //----------------------RANMDON 10-15-----------------------//
         int valorEntero = (int) Math.floor(Math.random() * ( (15 - 10) + 1) + 10);
         //----------------------------------------------------------//
-
+       
         //----------------------PORCENT OF SECTOR-----------------------//
         double rpta = table.size() * valorEntero / 100.0;
         //----------------------------------------------------------//
-        
+         System.out.println("15 porr "  +(int) rpta) ;
      
         //---------------------CREATE ARRAY SAMPLE--------------------//
         pixelSample[] dataSample = new pixelSample[ ((int) rpta)];
         //----------------------------------------------------------//
-        
+         System.out.println("15 xxxxx "  +dataSample.length) ;
       
         for (int i = 0; i < (dataSample.length) ; i++) {
             
-            int num = (int) Math.floor(Math.random() * (N - M + 1) + M);
-            if(num <= (dataSample.length) ){
+            int num = (int) Math.floor(Math.random() * dataSample.length);
+           
             Extrac.add((pixelSample) table.get(num));
-            }
+            
              
         }
         return Extrac;
